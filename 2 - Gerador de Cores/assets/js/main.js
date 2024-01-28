7845e6171c8cb260089a6d7ef512c992db275f5b
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2024-01-28 09:45:23 -0300

    Hello Word e geradores de cor

---
+++ 2 - Gerador de Cores/assets/js/main.js
@@ -0,0 +1,16 @@
+const botao = document.querySelector('.button');
+
+botao.addEventListener('click', e =>{
+    trocaCor()
+});
+
+function rand(x=1, y=255){
+    return Math.floor(Math.random() * (x + y) - x) 
+}
+
+function trocaCor(){
+    const fundo = document.querySelector('.fundo-cor')
+    fundo.style.backgroundColor = `rgb(${rand()}, ${rand()}, ${rand()})`
+}
+
+//console.log(rand(), rand(), rand()
