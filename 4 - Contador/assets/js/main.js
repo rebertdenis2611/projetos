ec8900544514139c80daf505527399b266497444
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2024-01-30 18:32:56 -0300

    Exercícios

--- 4 - Contador/assets/js/main.js
+++ 4 - Contador/assets/js/main.js
@@ -1,17 +1,30 @@
 const aumentar = document.querySelector('.aumentar')
 const diminuir = document.querySelector('.diminuir')
 const monitor = document.querySelector('.monitor')
-const qtdSoma = parseInt(document.querySelector('.qtd-number'))
-let contagem = 0 + qtdSoma.value
+const aumentarDiminuir = document.querySelector('.aumentar-ou-diminuir')
+let contagem = 0
+
 
 
 aumentar.addEventListener('click', () =>{
-    contagem += 1 
-    monitor.innerHTML = contagem 
+    contagem += 1
+    monitor.innerHTML = contagem
+    aumentarDiminuir.innerHTML = 'AUMENTANDO!!'
+    aumentarDiminuir.style.color = 'BLACK'
+
 })
 
 diminuir.addEventListener('click', () =>{
     if(contagem <= 0) return
     contagem -= 1 
     monitor.innerHTML = contagem
+    
+    if(contagem === 0){
+        aumentarDiminuir.innerHTML = 'ZEROU!!'
+        aumentarDiminuir.style.color = 'BLACK'
+    }else{
+        aumentarDiminuir.innerHTML = 'DIMINUINDO!!'
+        aumentarDiminuir.style.color = 'RED'
+    }
+    
 })
---
+++ 5 - SlideShow/assets/js/main.js
@@ -0,0 +1,18 @@
+const slide = document.querySelector('.slide')
+let imagem = [
+    'assets/img/1.jpg',
+    'assets/img/2.jpg'
+]
+
+let count = 0
+
+function mudarImagem(){
+    setTimeout(() =>{
+        count++
+        slide.src = imagem[count]
+    },1000)
+}
+
+
+    
+mudarImagem()
