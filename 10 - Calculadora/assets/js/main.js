3dfce16439f31885ec5a49b262f6872d86ddd150
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2024-02-08 18:41:06 -0300

    Calculadora

--- 10 - Calculadora/assets/js/main.js
+++ 10 - Calculadora/assets/js/main.js
@@ -1,12 +1,32 @@
-const botao = document.querySelector('.btn');
-const visor = document.querySelector('.visor')
+class calculadora{
+    constructor(){
 
-botao.addEventListener('click', () =>{
-    visor.innerHTML = botao.value
-})
+    }
 
+    capturaValorBotao(){
+        document.addEventListener('click', (e) =>{
+            const el = e.target;
+            if(el.classList.contains('btn-num')){
+                this.inserirDisplay(el.innerHTML);
+            }
+            if(el.classList.contains('btn-igual')){
+                console.log(this.inserirDisplay())
+            }
+        })
+    }
 
+    fazerCalculo(valor){
+        const calc = eval(valor);
+        console.log(calc);
+        return calc;
+    }
 
+    inserirDisplay(valor){
+        const display = document.querySelector('.display');
+        display.value += valor;
+        return(eval(display.value))
+    }
+}
 
-
-
+const calc = new calculadora();
+calc.capturaValorBotao()
